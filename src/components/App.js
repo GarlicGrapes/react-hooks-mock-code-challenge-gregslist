d1c539c6fc8447e0bc6e5735a1454fe4469437dd
Austin Longo <austinlongocontracting@gmail.com>
2022-08-01 23:49:54 -0400

    commit tree 10758f1620411120821212ce50ebf138e269cedf

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((res) => res.json())
+    .then((data) => {
+      setListings(data)
+      console.log(data)
+    })
+  }, [])
+  
+  function onDeleteClick(id) {
+    const updatedListings = listings.filter((listing) => listing.id !== id)
+    setListings(updatedListings)
+  }
+
+
+
+
+
+    const displayedListings = listings.filter((listing) => listing.description.includes(search))
+
+
+  
+
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header 
+        handleSearch={setSearch}
+      />
+      <ListingsContainer 
+        listings={displayedListings} 
+        setListings={setListings} 
+        onDeleteClick={onDeleteClick} 
+      />
     </div>
   );
 }
